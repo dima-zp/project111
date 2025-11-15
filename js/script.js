265ad02ecce22efded0b4dc907b60ebd87bedaf3
dima-zp <betsdima13@gmail.com>
2025-11-15 09:33:07 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,34 @@
+function scrollLeft() {
+  document.getElementById('slider').scrollBy({ left: -300, behavior: 'smooth' });
+}
+function scrollRight() {
+  document.getElementById('slider').scrollBy({ left: 300, behavior: 'smooth' });
+}
+// function overboxzapis() {
+//   document.getElementById("bookingForm").style.display = "block";
+// }
+
+// function closeForm() {
+//   document.getElementById("bookingForm").style.display = "none";
+// }
+function toggleMenu() {
+  document.getElementById("dropdown-content").classList.toggle("show");
+}
+
+function openFeedback() {
+  document.getElementById("feedbackModal").style.display = "block";
+}
+function closeFeedback() {
+  document.getElementById("feedbackModal").style.display = "none";
+}
+
+  // function overboxzapis() {
+  //   document.getElementById("bookingForm").style.display = "block";
+  // }
+
+  // function closeForm() {
+  //   document.getElementById("bookingForm").style.display = "none";
+  // }
+
+
+  
